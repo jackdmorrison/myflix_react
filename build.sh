f18d2f3d14892b3815ea65fc020b71588e219fbd
jack morrison <bobimnot123@gmail.com>
2022-11-29 12:03:12 +0000

    i love commiting to main

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 docker stop nodeserver 
 docker rm nodeserver 
-docker build -t nodeserv:latest .
+docker build -t nodeserv:1.0 .
 
-docker run -d -it -p 8080:80 --name nodeserver nodeserv
+docker run -d -it -p 8080:80 --name nodeserver nodeserv:1.0
