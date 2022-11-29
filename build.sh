f3513c5adcb43efcf364e05e9e473ac67f7fbede
jack morrison <bobimnot123@gmail.com>
2022-11-29 15:20:05 +0000

    how di that even happen

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ docker rm nodeserver
 cat password.txt | docker login --username jackmoinc --password-stdin
 docker build -t jackmoinc/myflix_react:remote .
 docker push jackmoinc/myflix_react:remote
-docker pull jackmoinc/mynetflix_react:remote
+docker pull jackmoinc/myflix_react:remote
 docker run -d -it -p 8080:80 --name nodeserver jackmoinc/myflix_react:remote
