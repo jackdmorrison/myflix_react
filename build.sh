d04a190117c7c0c85af94997e9737925b41985e2
jack morrison <bobimnot123@gmail.com>
2022-11-29 12:34:49 +0000

    possible fix

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 docker stop nodeserver 
 docker rm nodeserver 
-docker build -t jackmoinc/myflix_react:latest .
-docker push jackmoinc/myflix_react:latest
+docker build -t jackmoinc/myflix_react .
+docker push jackmoinc/myflix_react
 
-docker run -d -it -p 8080:80 --name nodeserver jackmoinc/myflix_react:latest
+docker run -d -it -p 8080:80 --name nodeserver jackmoinc/myflix_react
