cd9ceeda15e02c43c9ee682fa1a74376a9c7f1d0
jack morrison <bobimnot123@gmail.com>
2022-11-29 13:13:16 +0000

    ugggggggggggggghhhhhhhhhhhhhhhhhhhhhhhhhhhhhh

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@ docker stop nodeserver
 docker rm nodeserver 
 docker build -t jackmoinc/myflix_react:remote .
 docker push jackmoinc/myflix_react:remote
-
+docker pull jackmoinc/mynetflix_react:remote
 docker run -d -it -p 8080:80 --name nodeserver jackmoinc/myflix_react:remote
