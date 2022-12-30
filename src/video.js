bdf9885404e7c6ad34b15700c2c07461a35c01ae
jack morrison <bobimnot123@gmail.com>
2022-12-30 12:26:04 +0000

    updated path

--- src/video.js
+++ src/video.js
@@ -4,7 +4,7 @@ import 'bootstrap/dist/js/bootstrap.bundle.min';
 export default function video({video}) {
   return (
     <div class="card" style={{width: '18rem'}}>
-      <img src={video.picture} class="card-img-top" alt="{video.name}"/>
+      <img src="/thumbnails/"{...video.picture} class="card-img-top" alt={video.name}/>
       <div class="card-body">
         <h5 class="card-title">{video.Name}</h5>
         <p class="card-text">category: {video.category}</p>
