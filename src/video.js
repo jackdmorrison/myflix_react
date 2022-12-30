e854e7815260c70ca154490fd507af8a3b894db2
jack morrison <bobimnot123@gmail.com>
2022-12-30 19:45:32 +0000

    alright

--- src/video.js
+++ src/video.js
@@ -5,7 +5,7 @@ export default function video({video}) {
   const imgSRC="./Thumbnails/"+video.pic;
   return (
     <div class="card" style={{width: '18rem'}}>
-      <img src={imgSRC} class="card-img-top" alt={video.name}/>
+      <img src={require(imgSRC)} class="card-img-top" alt={video.name}/>
       <div class="card-body">
         <h5 class="card-title">{video.Name}</h5>
         <p class="card-text">category: {video.category}</p>
