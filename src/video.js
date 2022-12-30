c1d6b4a8e2c3ca368b4c77990c0d91ca70373291
jack morrison <bobimnot123@gmail.com>
2022-12-30 19:23:21 +0000

    fixup

--- src/video.js
+++ src/video.js
@@ -2,9 +2,10 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 export default function video({video}) {
+  const imgSRC="Thumbnails"+video.pic;
   return (
     <div class="card" style={{width: '18rem'}}>
-      <img src="/Thumbnails/"{...video.pic} class="card-img-top" alt={video.name}/>
+      <img src={imgSRC} class="card-img-top" alt={video.name}/>
       <div class="card-body">
         <h5 class="card-title">{video.Name}</h5>
         <p class="card-text">category: {video.category}</p>
