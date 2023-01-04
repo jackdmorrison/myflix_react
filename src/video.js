d9e2cd40f56c4c232ca5ee0887e907df458f9dec
jack morrison <bobimnot123@gmail.com>
2023-01-04 23:25:21 +0000

    Update video.js

--- src/video.js
+++ src/video.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
+import { eventWrapper } from '@testing-library/user-event/dist/utils';
 export default function video({video}) {
   //const imgSRC="./"+video.pic;
-  let imgSRC=require("./Thumbnails/"+video.video.pic);
+  
+  let imgSRC=require("./Thumbnails/"+video.video.thumb);
+  
   return (
     <div className="card" style={{width: '18rem'}}>
       <img src={imgSRC} className="card-img-top" alt={video.video.name}/>
